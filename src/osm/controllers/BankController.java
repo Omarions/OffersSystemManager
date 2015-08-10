26d4d97814e633e8ec5685fd3b75e6c9fd5193c1
Omarions <eng.omarfawzy@gmail.com>
2015-08-10 14:54:19 +0200

    Add doc to "BankController" class and finish its "add, edit and remove" methods

--- src/osm/controllers/BankController.java
+++ src/osm/controllers/BankController.java
@@ -19,20 +19,21 @@ import osm.models.Bank;
  * @author Omar
  */
 public class BankController {
+
     private Bank bank;
-    private int genBankID= -1;
+    private int genBankID = -1;
     private ResultSet rs;
     private PreparedStatement stmt;
-    
-    public Bank getBankByID(int bankID){
+
+    public Bank getBankByID(int bankID) {
         //TODO
         //return company that has sepcified ID.
         try {
-            String query="SELECT * FROM bank WHERE bank_id=?";
-            stmt=Connector.open().prepareStatement(query);
+            String query = "SELECT * FROM bank WHERE bank_id=?";
+            stmt = Connector.open().prepareStatement(query);
             stmt.setInt(1, bankID);
-            rs=stmt.executeQuery();
-            while(rs.next()){
+            rs = stmt.executeQuery();
+            while (rs.next()) {
                 bank.setBankId(bankID);
                 bank.setName(rs.getString("name"));
                 bank.setAccountName(rs.getString("account_name"));
@@ -42,15 +43,15 @@ public class BankController {
             }
         } catch (SQLException ex) {
             Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
-            if(rs!=null){
+        } finally {
+            if (rs != null) {
                 try {
                     rs.close();
                 } catch (SQLException ex) {
                     Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            if(stmt!=null){
+            if (stmt != null) {
                 try {
                     stmt.close();
                 } catch (SQLException ex) {
@@ -60,36 +61,38 @@ public class BankController {
             Connector.close();
         }
         return bank;
-   } 
-   
-   public int addBank(Bank newBank){
-       //TODO
-       //add a new company to db and return true if added successfuly and false if not.
-       
-       String query="INSERT INTO bank (bank_id,name,account_name,account_number,swift_code,currency) VALUES(0,?,?,?,?,?)";
+    }
+
+    /*add a new company to db and return true if added successfuly and false if not.
+    *@parameter Bank newBank is the new bank data to be added
+    *@return int genBankID is the generated bank id for new record bank.
+    */
+    public int addBank(Bank newBank) {
+
+        String query = "INSERT INTO bank (bank_id,name,account_name,account_number,swift_code,currency) VALUES(0,?,?,?,?,?)";
         try {
-            stmt=Connector.open().prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+            stmt = Connector.open().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, newBank.getName());
             stmt.setString(2, newBank.getAccountName());
             stmt.setString(3, newBank.getAccountNumber());
             stmt.setString(4, newBank.getSwiftCode());
             stmt.setString(5, newBank.getCurrency());
             stmt.executeUpdate();
-            rs=stmt.getGeneratedKeys();
-            if(rs.next()){
-                genBankID=rs.getInt(1);
+            rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                genBankID = rs.getInt(1);
             }
         } catch (SQLException ex) {
             Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
-            if(rs!=null){
+        } finally {
+            if (rs != null) {
                 try {
                     rs.close();
                 } catch (SQLException ex) {
                     Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            if(stmt!=null){
+            if (stmt != null) {
                 try {
                     stmt.close();
                 } catch (SQLException ex) {
@@ -98,20 +101,68 @@ public class BankController {
             }
             Connector.close();
         }
+
+        return genBankID;
+    }
+
+    /*edit an old offer with the new one and return the new offer.
+    *@parameter int bankId is the id of bank to be modified
+    *@return int rowsAffected is the affected rows by this operation.
+    */
+    public int editOffer(int bankId, Bank newBank) {
        
-       return genBankID;
-   }
-   
-   public Bank editOffer(Bank oldBank, Bank newBank){
-       //TODO
-       //edit an old offer with the new one and return the new offer.
-       return null;
-   }
-   
-   public boolean RemoveCompany(int bankID){
-       //TODO
-       //remove an sepecified offer with RefNo. return true if successful.
-       return false;
-   }
+        int rowsAffected = -1;
+        String query = "UPDATE TABLE bank SET name=?, account_name=?, account_number=?, swift_code=?, currency=? WHERE bank_id=?";
+        try {
+            stmt = Connector.open().prepareStatement(query);
+            stmt.setString(1, newBank.getName());
+            stmt.setString(2, newBank.getAccountName());
+            stmt.setString(3, newBank.getAccountNumber());
+            stmt.setString(4, newBank.getSwiftCode());
+            stmt.setString(5, newBank.getCurrency());
+            stmt.setInt(6, bankId);
+            rowsAffected = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            Connector.close();
+        }
+
+        return rowsAffected;
+    }
+
+    /*remove an sepecified offer with RefNo. return true if successful. 
+    * @parameter int bankId is the id of bank to be removed
+    * @return int rowsAffected is the affected rows by this operation.
+    */
+    public int RemoveBank(int bankId) {
+       
+        int rowsAffected = -1;
+        String query = "DELETE FROM bank WHERE bank_id=?";
+        try {
+            stmt = Connector.open().prepareStatement(query);
+            stmt.setInt(1, bankId);
+            rowsAffected = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(BankController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            Connector.close();
+        }
+        return rowsAffected;
+    }
 
 }
